0b00062a8033c33bac5ba40d505ef0567212cf4e
Samrinder <samrinder165@gmail.com>
2025-01-30 15:22:50 -0500

    comment

---
+++ Javascript/Week 3/demo.js
@@ -0,0 +1,42 @@
+//for (let x = 0; x < 10; x++) {
+ //   console.log(x);
+    
+//}
+
+//for (let x = 0; x < 13; x++) {
+//    console.log(x, "x 5 =", x*5);
+    
+//}
+
+//for (let x = 0; x < 15; x++) {
+ //   console.log(x,"x",factor,"=",x*factor);
+    
+//}
+//let students = ["John", "Jane", "Sam", "Jason"]
+//for (let index = 0; index < students.length; index++) {
+//    console.log(students[index]); 
+    
+//}
+
+//for (const element of object) {
+//    console.log(element)
+//}
+
+//let car = {make: "Toyota", model: "Corolla", year: "2020"};
+//for (const key in car) {
+//    console.log( $[key],$(car[key]));
+    
+//    }
+let factor = 5;
+let x = 1;
+while (x < 13) {
+    console.log(x,"x",factor,"=",x*factor);
+    x++;
+    
+}
+
+//let cups = 0;
+//do {
+//    console.log('Cup of coffee number: $[')
+    
+//} while (condition);
---
+++ Javascript/lab2.js
@@ -0,0 +1,91 @@
+// TASK 1 CONDITIONAL STATEMENTS
+
+//let x = 10;
+
+//if (x > 0) {
+//    console.log("The number is positive")
+//}
+//else if (x < 0) {
+//    console.log("The number is negative ")
+//}
+//else {
+//    console.log("The number is zero")
+//}
+
+//let Grade = "A"
+//switch (Grade) {
+//    case "A":
+//        console.log("Excellent")
+//        break;
+//    case "B":
+//        console.log("Very Good")
+//        break;
+//    case "C":
+//        console.log("Good")
+//        break;
+//    case "D":
+//        console.log("Just Pass")
+//        break;
+//    case "F":
+//        console.log("Fail")      
+//    default:
+//        console.log("Invalid Grade")
+             
+//}
+
+// TASK 2 LOOP STATEMENTS
+
+//let i = 1;
+//while (i <=10) {
+//    console.log(i);
+//    i++
+//}
+
+
+//let value = 1;
+//do {
+//    console.log( value*3)
+//    value++
+//} while (value <= 5);
+
+
+//let colours = ['Green', 'Blue', 'Yellow', 'White', 'Black']
+
+//for (let x = 0; x < colours.length; x++) {
+//    console.log(colours[x]);
+    
+//}
+
+
+
+// TASK 3 ITERATION STATEMENTS
+
+//let person = {name:"Samrinder",Age:"18",Birthplace:"Canada" }
+
+//for (let key in person) {
+//        console.log(`${key}:${person[key]}`);
+        
+//    }
+
+//let grades = ['A','B','C','D','F']
+//for (let grade of grades) {
+//    console.log(grade)
+    
+//}
+
+
+//TASK 4 BREAK AND CONTINUE STATEMENTS
+
+//for (let x = 0; x <= 10; x++){
+//    if (x === 5)  
+//    break;
+//    console.log(x);
+//}
+
+//const arrayofnumbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+//for (let x = 0; x < arrayofnumbers.length; x++){
+//    if (arrayofnumbers[x] % 2 != 0) continue;
+//    console.log(arrayofnumbers[x])
+//}
+
+
